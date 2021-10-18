b2e454b3f0ff8e9394fe6e59fc6bf7cf0be7b096
Vadim Volkov <volvadvit@gmail.com>
2021-10-18 22:15:35 +0300

    - Add Count sort

---
+++ src/main/java/com/volvadvit/sort/Count/Count.java
@@ -0,0 +1,51 @@
+package com.volvadvit.sort.Count;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Count {
+
+    public static void main(String[] args) {
+        // Set Up
+        int[] unsorted = createStartArrayWithLength(50);
+        int maxValue = getMaxFromArray(unsorted);
+
+        System.err.println(countSort(unsorted, maxValue));
+    }
+
+    private static List<Integer> countSort(int[] unsorted, int max) {
+        // Fill count array
+        int[] countArray = new int[max + 1];
+        Arrays.fill(countArray, 0);
+        for (int num : unsorted) {
+            countArray[num]++;
+        }
+
+        // Fill result list
+        List<Integer> result = new ArrayList<>();
+        for (int num = 0; num < countArray.length; num++) {
+            for (int i = 0; i < countArray[num]; i++) {
+                result.add(num);
+            }
+        }
+        return result;
+    }
+
+    private static int getMaxFromArray(int[] array) {
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (max < array[i]) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+    private static int[] createStartArrayWithLength(int length) {
+        int[] ar = new int[length];
+        for (int i = 0; i < length; i++) {
+            ar[i] = (int) (Math.random() * 100);
+        }
+        return ar;
+    }
+}
